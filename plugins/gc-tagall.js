fabfb7fbb50d243da9446d7e7bca9519ea2fc573
Nayee22 <140297710+Nayee22@users.noreply.github.com>
2023-09-18 22:57:59 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -10,7 +10,7 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   for (const mem of participants) {
     teks += `🌷 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* SUN VENTAS \n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += `*└* Sun Ventas \n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
